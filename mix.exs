3ede2e96ee4e1b8a593dc8fafbcb7dcc53c6a705
gitneko <67227083+gitneko@users.noreply.github.com>
2025-08-08 23:13:46 +0200

    Remove extra application in mix.exs

--- mix.exs
+++ mix.exs
@@ -35,9 +35,7 @@ defmodule BACnetEDE.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
